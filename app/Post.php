7b520dbf0061e37c4ac53b1f60bab669c08cc381
Samuel Cole <cole.samuel21@gmail.com>
2018-01-24 17:15:55 +0000

    begin implementing post categories

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $table = 'posts';
     
+    /**
+     * Define relationship between posts and categories.
+     *
+     * @return eloquent relationship
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category', 'category_id');
+    }
 }
