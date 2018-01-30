f46198f48afcf0352406436def8c223028f2290b
Samuel Cole <cole.samuel21@gmail.com>
2018-01-30 19:24:06 +0000

    implemented DESTROY functionality for categories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(PostsSeeder::class);
+        $this->call(PostsSeeder::class);
         $this->call(CategoriesSeeder::class);
     }
 }
